17800e6a7e4e8750ddb17900ab305f664aceb3b4
Gytis <lile271@gmail.com>
2019-08-29 08:35:02 +0300

    Added javadoc

--- GroupProject/src/lt/viko/eif/groupwork/FileUploadService.java
+++ GroupProject/src/lt/viko/eif/groupwork/FileUploadService.java
@@ -9,6 +9,12 @@ import javax.ws.rs.core.MediaType;
 import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
 import org.glassfish.jersey.media.multipart.FormDataParam;
 
+/**
+*
+* Class used for file uploading
+*/
+
+
 @Path("/files")
 public class FileUploadService {
 	
@@ -18,6 +24,16 @@ public class FileUploadService {
     @Path("/upload")
     @Consumes(MediaType.MULTIPART_FORM_DATA)
     public String uploadFile (
+/**
+    		   * Method that allows us to upload file to database by ID
+    		   *
+    		   * @param id  allows us to find files by ID
+    		   * @param uploadedInputStream 
+    		   * @param fileDetails
+    		   * @param password string, which we use for file encryption
+    		   * @return Saves file to database
+    		   */
+
             @FormDataParam("file") InputStream uploadedInputStream,
             @FormDataParam("file") FormDataContentDisposition fileDetails,
             @FormDataParam("id") String id,
